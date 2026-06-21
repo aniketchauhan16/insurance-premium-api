0b55bc225de49aa07bb0fb78fe98459c98f9520e
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-06-21 15:27:18 +0530

    Finished working on our Pydantic model

--- app.py
+++ app.py
@@ -10,6 +10,20 @@ with open('model.pkl','rb') as f:
 
 app = FastAPI()
 
+tier_1_cities = [
+    "Mumbai", "Delhi", "Bangalore", "Chennai", 
+    "Kolkata", "Hyderabad", "Pune"
+]
+
+tier_2_cities = [
+    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
+    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
+    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
+    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
+    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
+    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
+]
+
 #building pydantic model to validate incoming data
 
 class UserInput(BaseModel):
@@ -20,3 +34,43 @@ class UserInput(BaseModel):
     smoker: Annotated[bool, Field(..., description= 'Is the user a smoker')]
     city:Annotated[str, Field(..., description= 'city of the user')]
     occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job','business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return self.weight/(self.height**2)
+    
+    @computed_field
+    @property
+    def lifestyle_risk(self) -> str:
+        if self.smoker and self.bmi > 30:
+            return "high"
+        elif self.smoker or self.bmi > 27:
+            return "medium"
+        else:
+            return "low"  
+        
+    @computed_field
+    @property
+    def age_group(self) -> str:
+        if self.age < 25:
+         return "young"
+        elif self.age < 45:
+          return "adult"
+        elif self.age < 60:
+         return "middle_aged"
+        
+        return "senior"
+
+    @computed_field
+    @property
+    def city_tier(self)-> int:
+       if self.city in tier_1_cities:
+            return 1
+       elif self.city in tier_2_cities:
+            return 2
+       else:
+            return 3
+
+I
+            
